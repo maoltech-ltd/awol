b6ea645e5fc8e8268c2f6642a91617862d31fa85
HP <hardeyshinnah.mo@gmail.com>
2026-04-04 13:24:33 +0100

    feat: pages

--- src/app/(about)/about/page.tsx
+++ src/app/(about)/about/page.tsx
@@ -1,20 +1,48 @@
+// import AboutCoverSection from "@/src/components/About/AboutCoverSection";
+// import WhatsAppButton from "@/src/components/Buttons/WhatsAppButton";
+// import Link from "next/link";
+
+// const About = () => {
+//   return (
+//     <>
+//       <AboutCoverSection />
+
+//       <h2 className="mt-8 font-semibold text-lg md:text-2xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
+//         Interested in our EVs, solar systems, inverters, or other electronics?
+//         ⚡ Contact us{" "}
+//         <WhatsAppButton message={"Hello, I am interested in learning more about your products and services."} />{" "}
+//         and our team will help you find the perfect energy or electronics
+//         solution for your home or business.
+//       </h2>
+//     </>
+//   );
+// };
+
+// export default About;
+
 import AboutCoverSection from "@/src/components/About/AboutCoverSection";
-import Link from "next/link";
+import WhatsAppButton from "@/src/components/Buttons/WhatsAppButton";
 
 const About = () => {
   return (
     <>
       <AboutCoverSection />
 
-      <h2 className="mt-8 font-semibold text-lg md:text-2xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
-        Interested in our EVs, solar systems, inverters, or other electronics?
-        ⚡ Contact us{" "}
-        <Link href="/contact" className="!underline underline-offset-2">
-          here
-        </Link>{" "}
-        and our team will help you find the perfect energy or electronics
-        solution for your home or business.
-      </h2>
+      <div className="mx-5 sm:mx-10 md:mx-16 lg:mx-20 mt-8">
+        <h2 className="font-semibold text-lg md:text-2xl text-dark dark:text-light">
+          Interested in our EVs, solar systems, inverters, or other electronics?
+          ⚡
+        </h2>
+
+        <p className="mt-4 text-base md:text-lg">
+          Contact us directly on WhatsApp and our team will help you find the
+          perfect energy solution for your home or business.
+        </p>
+
+        <div className="mt-4">
+          <WhatsAppButton message="Hello, I am interested in learning more about your products and services." />
+        </div>
+      </div>
     </>
   );
 };
